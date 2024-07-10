26a961940b8e0f5b26a5e216109f0df26019216e
Melanie <melanie.mazanec@gmail.com>
2024-07-10 16:54:13 -0400

    Update annotation

--- app/models/state_file_nj_intake.rb
+++ app/models/state_file_nj_intake.rb
@@ -3,15 +3,15 @@
 # Table name: state_file_nj_intakes
 #
 #  id                                :bigint           not null, primary key
-#  account_type                      :integer          default(0), not null
+#  account_type                      :integer          default("unfilled"), not null
 #  claimed_as_dep                    :integer
-#  consented_to_terms_and_conditions :integer          default(0), not null
-#  contact_preference                :integer          default(0), not null
+#  consented_to_terms_and_conditions :integer          default("unfilled"), not null
+#  contact_preference                :integer          default("unfilled"), not null
 #  current_sign_in_at                :datetime
 #  current_sign_in_ip                :inet
 #  current_step                      :string
-#  eligibility_lived_in_state        :integer          default(0), not null
-#  eligibility_out_of_state_income   :integer          default(0), not null
+#  eligibility_lived_in_state        :integer          default("unfilled"), not null
+#  eligibility_out_of_state_income   :integer          default("unfilled"), not null
 #  email_address                     :citext
 #  email_address_verified_at         :datetime
 #  failed_attempts                   :integer          default(0), not null
@@ -22,7 +22,7 @@
 #  last_sign_in_ip                   :inet
 #  locale                            :string           default("en")
 #  locked_at                         :datetime
-#  payment_or_deposit_type           :integer          default(0), not null
+#  payment_or_deposit_type           :integer          default("unfilled"), not null
 #  permanent_apartment               :string
 #  permanent_city                    :string
 #  permanent_street                  :string
@@ -30,7 +30,7 @@
 #  phone_number                      :string
 #  phone_number_verified_at          :datetime
 #  primary_dob                       :date
-#  primary_esigned                   :integer          default(0), not null
+#  primary_esigned                   :integer          default("unfilled"), not null
 #  primary_first_name                :string
 #  primary_last_name                 :string
 #  primary_middle_initial            :string
@@ -40,7 +40,7 @@
 #  sign_in_count                     :integer          default(0), not null
 #  source                            :string
 #  spouse_dob                        :date
-#  spouse_esigned                    :integer          default(0), not null
+#  spouse_esigned                    :integer          default("unfilled"), not null
 #  spouse_first_name                 :string
 #  spouse_last_name                  :string
 #  spouse_middle_initial             :string
@@ -51,8 +51,15 @@
 #  visitor_id                        :string
 #
 class StateFileNjIntake < StateFileBaseIntake
+
+  def disqualifying_eligibility_rules
+    {
+      eligibility_lived_in_state: "no",
+    }
+  end
+
   def tax_calculator(include_source: false)
-      Efile::Wa::Wa1000.new(
+      Efile::Nj::Nj1040.new(
         year: MultiTenantService.statefile.current_tax_year,
         intake: self,
         include_source: include_source
