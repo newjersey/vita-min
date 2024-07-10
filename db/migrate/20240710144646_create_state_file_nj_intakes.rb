3e512112b41c3ddce87cf1872de1b01165f36593
Melanie <melanie.mazanec@gmail.com>
2024-07-10 11:18:49 -0400

    Add NJ intake model and db table

---
+++ db/migrate/20240710144646_create_state_file_nj_intakes.rb
@@ -0,0 +1,69 @@
+class CreateStateFileNjIntakes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :state_file_nj_intakes do |t|
+      # Mostly copied from Warshington example, also from NY
+
+      # misc
+      t.integer :tax_return_year
+      t.integer :filing_status
+      t.string :current_step
+      t.text :raw_direct_file_data
+
+      # personal info
+      t.string :primary_first_name
+      t.string :primary_middle_initial
+      t.string :primary_last_name
+      t.date :primary_dob
+      t.string :primary_ssn
+
+      t.string :spouse_first_name
+      t.string :spouse_middle_initial
+      t.string :spouse_last_name
+      t.date :spouse_dob
+      t.string :spouse_ssn
+
+      t.string :permanent_street
+      t.string :permanent_apartment
+      t.string :permanent_city
+      t.string :permanent_zip
+
+      # browser info
+      t.string :source
+      t.string :referrer
+      t.string :locale, default: 'en'
+      t.string :visitor_id
+
+      # Tax info
+      t.integer :claimed_as_dep
+      t.integer :fed_wages
+      t.integer :fed_taxable_income
+
+      # contact info
+      t.citext :email_address
+      t.string :phone_number
+      t.datetime :email_address_verified_at
+      t.datetime :phone_number_verified_at
+
+      # enums
+      t.integer :contact_preference, default: 0, null: false
+      t.integer :eligibility_lived_in_state, default: 0, null: false
+      t.integer :eligibility_out_of_state_income, default: 0, null: false
+      t.integer :primary_esigned, default: 0, null: false
+      t.integer :spouse_esigned, default: 0, null: false
+      t.integer :account_type, default: 0, null: false
+      t.integer :payment_or_deposit_type, default: 0, null: false
+      t.integer :consented_to_terms_and_conditions, default: 0, null: false
+
+      # devise
+      t.integer :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet :last_sign_in_ip
+      t.inet :current_sign_in_ip
+      t.integer :failed_attempts, default: 0, null: false
+      t.datetime :locked_at
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/state_file_nj_intakes.rb
@@ -0,0 +1,57 @@
+# == Schema Information
+#
+# Table name: state_file_nj_intakes
+#
+#  id                                :bigint           not null, primary key
+#  account_type                      :integer          default(0), not null
+#  claimed_as_dep                    :integer
+#  consented_to_terms_and_conditions :integer          default(0), not null
+#  contact_preference                :integer          default(0), not null
+#  current_sign_in_at                :datetime
+#  current_sign_in_ip                :inet
+#  current_step                      :string
+#  eligibility_lived_in_state        :integer          default(0), not null
+#  eligibility_out_of_state_income   :integer          default(0), not null
+#  email_address                     :citext
+#  email_address_verified_at         :datetime
+#  failed_attempts                   :integer          default(0), not null
+#  fed_taxable_income                :integer
+#  fed_wages                         :integer
+#  filing_status                     :integer
+#  last_sign_in_at                   :datetime
+#  last_sign_in_ip                   :inet
+#  locale                            :string           default("en")
+#  locked_at                         :datetime
+#  payment_or_deposit_type           :integer          default(0), not null
+#  permanent_apartment               :string
+#  permanent_city                    :string
+#  permanent_street                  :string
+#  permanent_zip                     :string
+#  phone_number                      :string
+#  phone_number_verified_at          :datetime
+#  primary_dob                       :date
+#  primary_esigned                   :integer          default(0), not null
+#  primary_first_name                :string
+#  primary_last_name                 :string
+#  primary_middle_initial            :string
+#  primary_ssn                       :string
+#  raw_direct_file_data              :text
+#  referrer                          :string
+#  sign_in_count                     :integer          default(0), not null
+#  source                            :string
+#  spouse_dob                        :date
+#  spouse_esigned                    :integer          default(0), not null
+#  spouse_first_name                 :string
+#  spouse_last_name                  :string
+#  spouse_middle_initial             :string
+#  spouse_ssn                        :string
+#  tax_return_year                   :integer
+#  created_at                        :datetime         not null
+#  updated_at                        :datetime         not null
+#  visitor_id                        :string
+#
+FactoryBot.define do
+  factory :state_file_nj_intake do
+    
+  end
+end
